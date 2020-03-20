4150f5b7a89c3c6beb0c8b5fb4012cce40e830f8
sund4e <suvi@smartly.io>
2020-03-20 10:22:30 +0200

    TextInput and Container components

---
+++ app/app.tsx
@@ -0,0 +1,78 @@
+import React, { useReducer, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import API, { graphqlOperation } from '@aws-amplify/api';
+import PubSub from '@aws-amplify/pubsub';
+import { createTodo } from '../src/graphql/mutations';
+import { CreateTodoInput } from '../src/API';
+import { listTodos } from '../src/graphql/queries';
+import TextInput from './components/textInput/textInput';
+import Container from './components/container/container';
+import { colors } from './styles';
+
+import config from '../aws-exports';
+API.configure(config); // Configure Amplify
+PubSub.configure(config);
+
+const reducer = (state, action): { todos: CreateTodoInput[] } => {
+  switch (action.type) {
+    case 'QUERY':
+      return { ...state, todos: action.todos };
+    case 'SUBSCRIPTION':
+      return { ...state, todos: [...state.todos, action.todo] };
+    default:
+      return state;
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.dark,
+    padding: 20,
+    justifyContent: 'center',
+    borderColor: 'red',
+    borderWidth: 1,
+    fontSize: 20
+  }
+});
+
+export default function App(): React.ReactElement {
+  const [state, dispatch] = useReducer(reducer, { todos: [] });
+
+  async function getData(): Promise<void> {
+    const todoData = await API.graphql(graphqlOperation(listTodos));
+    dispatch({ type: 'QUERY', todos: todoData.data.listTodos.items });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const click = async (): Promise<void> => {
+    const todo: CreateTodoInput = {
+      name: 'testing',
+      description: 'just testing'
+    };
+    await API.graphql(graphqlOperation(createTodo, { input: todo }));
+    await getData();
+  };
+
+  console.log('hello');
+
+  return (
+    <View style={styles.container}>
+      <Text>Testing what happens :D</Text>
+      <Container>
+        <TextInput />
+        <TextInput />
+        <TextInput />
+      </Container>
+      <Button onPress={click} title={'Press me'} />
+      {state.todos.map(todo => (
+        <Text key={todo.id}>
+          {todo.name} : {todo.description}
+        </Text>
+      ))}
+    </View>
+  );
+}
---
+++ app/components/textInput/textInput.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { TextInput as NativeInput, StyleSheet } from 'react-native';
+import { colors } from '../../styles';
+
+const styles = StyleSheet.create({
+  empty: {
+    borderColor: colors.light,
+    color: colors.light,
+    backgroundColor: 'transparent'
+  },
+  focused: {
+    borderColor: colors.accent,
+    color: colors.accent,
+    backgroundColor: colors.primary
+  },
+  unfocused: {
+    borderColor: colors.highlight,
+    color: colors.highlight,
+    backgroundColor: colors.primary
+  },
+  input: {
+    padding: 10,
+    height: 40,
+    borderWidth: 1,
+    margin: 10,
+    borderRadius: 5,
+    fontSize: 18
+  }
+});
+
+export default function TextInput(): React.ReactElement {
+  const [value, onChangeText] = React.useState('TEsting');
+  const inputRef = React.useRef<NativeInput>();
+
+  const onBlur = (): void => {
+    const isEmpty = value === '';
+    if (inputRef.current) {
+      inputRef.current.setNativeProps({
+        style: isEmpty ? styles.empty : styles.unfocused
+      });
+    }
+  };
+
+  const onFocus = (): void => {
+    if (inputRef.current) {
+      inputRef.current.setNativeProps({
+        style: styles.focused
+      });
+    }
+  };
+
+  return (
+    <NativeInput
+      ref={inputRef}
+      style={{ ...styles.input, ...styles.unfocused }}
+      onChangeText={(text): void => onChangeText(text)}
+      placeholder={'Name your move'}
+      placeholderTextColor={colors.light}
+      value={value}
+      onBlur={onBlur}
+      onFocus={onFocus}
+    />
+  );
+}
